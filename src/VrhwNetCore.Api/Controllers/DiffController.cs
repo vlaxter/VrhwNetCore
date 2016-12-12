43136f13019d3a54b99691a3951708c178c95ec4
Vladimir.Rivera <Vladimir.Rivera@9217T32>
2016-12-11 21:52:47 -0500

    Created DotNet core version of Vrhw

--- src/VrhwNetCore.Api/Controllers/DiffController.cs
+++ src/VrhwNetCore.Api/Controllers/DiffController.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using VrhwNetCore.Shared.Interfaces;
 using VrhwNetCore.Shared.Models;
 
--- src/VrhwNetCore.Api/Startup.cs
+++ src/VrhwNetCore.Api/Startup.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using VrhwNetCore.Core.Services;
+using VrhwNetCore.Repository.Memory;
+using VrhwNetCore.Shared.Interfaces;
 
 namespace VrhwNetCore.Api
 {
     public class Startup
     {
+        // Simple Injector Core Net https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html
+        private Container container = new Container();
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -29,15 +36,43 @@ namespace VrhwNetCore.Api
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(new SimpleInjectorViewComponentActivator(container));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseSimpleInjectorAspNetRequestScoping(container);
+
+            container.Options.DefaultScopedLifestyle = new AspNetRequestLifestyle();
+
+            InitializeContainer(app);
+
+            container.Verify();
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            // Add application services. For instance:
+            container.Register<IDiffService, DiffService>(Lifestyle.Scoped);
+            //container.Register<IDiffRepository, SqlRepository>(Lifestyle.Scoped);
+            container.Register<IDiffRepository, MemoryRepository>(Lifestyle.Scoped);
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
+            // NOTE: Prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
         }
     }
 }
